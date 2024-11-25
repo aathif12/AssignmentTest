b48ead8b9685ea34e27f5c944034d4f2043f5c72
aathif12 <aathifahamad4@gmail.com>
2024-11-25 21:19:01 +0530

    table Basic

--- src/Components/StudentTable.js
+++ src/Components/StudentTable.js
@@ -1,3 +1,25 @@
-export default function StudentTable() {
-  return <div></div>;
+export default function StudentTable(props) {
+  return (
+    <>
+      <table width="100%" border="1">
+        <thead>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Course</th>
+          <th>Profile</th>
+        </thead>
+        <tbody>
+          {props.students.map((student) => (
+            <tr>
+              <td>{student.firstName}</td>
+              <td>{student.lastName}</td>
+              <td>{student.course}</td>
+              <td>{student.address.country}</td>
+            </tr>
+          ))}
+          <td></td>
+        </tbody>
+      </table>
+    </>
+  );
 }
